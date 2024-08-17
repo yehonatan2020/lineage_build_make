stellastructure-aosp/platform_build

Fix release configuration in build system once and for all.
Some autocompleting shells such as ble.sh cause problems when
typing out the command to manually lunch (I.e. we get lots of
invalid TARGET_RELEASE errors just as we type "lunch aosp_ar" for
autocomplete to suggest "lunch aosp_arm-ap1a-eng"). These problems
cause "lunch" to malfunction and render it unable to provide
autocompletion suggestions.

Signed-off-by: Stella Bloom <windowz414@gnuweeb.org>

core/release_config.mk
@@ -102,7 +102,7 @@ ifeq ($(TARGET_RELEASE),)
     # Instead of leaving this string empty, we want to default to a valid
     # setting.  Full builds coming through this path is a bug, but in case
     # of such a bug, we want to at least get consistent, valid results.
-    TARGET_RELEASE = trunk_staging
+    TARGET_RELEASE = ap1a
 endif
 
 ifeq ($(filter $(_all_release_configs), $(TARGET_RELEASE)),)

envsetup.sh
@@ -792,8 +792,8 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_arm-trunk_staging-eng]"
-        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
+        echo "Which would you like? [aosp_arm-ap1a-eng]"
+        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-ap1a-eng): "
         read answer
         used_lunch_menu=1
     fi
@@ -802,7 +802,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_arm-trunk_staging-eng
+        selection=aosp_arm-ap1a-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= get_build_var COMMON_LUNCH_CHOICES))
@@ -910,7 +910,7 @@ function tapas()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local arch="$(echo $* | xargs -n 1 echo | \grep -E '^(arm|x86|arm64|x86_64)$' | xargs)"
     # TODO(b/307975293): Expand tapas to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap1a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local density="$(echo $* | xargs -n 1 echo | \grep -E '^(ldpi|mdpi|tvdpi|hdpi|xhdpi|xxhdpi|xxxhdpi|alldpi)$' | xargs)"
     local keys="$(echo $* | xargs -n 1 echo | \grep -E '^(devkeys)$' | xargs)"
@@ -983,7 +983,7 @@ function banchan()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local product="$(echo $* | xargs -n 1 echo | \grep -E '^(.*_)?(arm|x86|arm64|riscv64|x86_64|arm64only|x86_64only)$' | xargs)"
     # TODO: Expand banchan to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap1a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local apps="$(echo $* | xargs -n 1 echo | \grep -E -v '^(user|userdebug|eng|(.*_)?(arm|x86|arm64|riscv64|x86_64))$' | xargs)"
 

yehonatan2020/lineage_build_make

core/release_config.mk
@@ -138,7 +138,7 @@ ifeq ($(TARGET_RELEASE),)
     # Instead of leaving this string empty, we want to default to a valid
     # setting.  Full builds coming through this path is a bug, but in case
     # of such a bug, we want to at least get consistent, valid results.
-    TARGET_RELEASE = trunk_staging
+    TARGET_RELEASE = ap2a
 endif
 
 ifeq ($(filter $(_all_release_configs), $(TARGET_RELEASE)),)

envsetup.sh
@@ -787,8 +787,8 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo "Which would you like? [aosp_cf_x86_64_phone-trunk_staging-eng]"
-        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-trunk_staging-eng): "
+        echo "Which would you like? [aosp_cf_x86_64_phone-ap2a-eng]"
+        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-ap2a-eng): "
         read answer
         used_lunch_menu=1
     fi
@@ -797,7 +797,7 @@ function lunch()
 
     if [ -z "$answer" ]
     then
-        selection=aosp_cf_x86_64_phone-trunk_staging-eng
+        selection=aosp_cf_x86_64_phone-ap2a-eng
     elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
     then
         local choices=($(TARGET_BUILD_APPS= TARGET_PRODUCT= TARGET_RELEASE= TARGET_BUILD_VARIANT= get_build_var COMMON_LUNCH_CHOICES 2>/dev/null))
@@ -928,7 +928,7 @@ function tapas()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local arch="$(echo $* | xargs -n 1 echo | \grep -E '^(arm|x86|arm64|x86_64)$' | xargs)"
     # TODO(b/307975293): Expand tapas to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap2a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local density="$(echo $* | xargs -n 1 echo | \grep -E '^(ldpi|mdpi|tvdpi|hdpi|xhdpi|xxhdpi|xxxhdpi|alldpi)$' | xargs)"
     local keys="$(echo $* | xargs -n 1 echo | \grep -E '^(devkeys)$' | xargs)"
@@ -1001,7 +1001,7 @@ function banchan()
     local showHelp="$(echo $* | xargs -n 1 echo | \grep -E '^(help)$' | xargs)"
     local product="$(echo $* | xargs -n 1 echo | \grep -E '^(.*_)?(arm|x86|arm64|riscv64|x86_64|arm64only|x86_64only)$' | xargs)"
     # TODO: Expand banchan to take release arguments (and update hmm() usage).
-    local release="trunk_staging"
+    local release="ap2a"
     local variant="$(echo $* | xargs -n 1 echo | \grep -E '^(user|userdebug|eng)$' | xargs)"
     local apps="$(echo $* | xargs -n 1 echo | \grep -E -v '^(user|userdebug|eng|(.*_)?(arm|x86|arm64|riscv64|x86_64))$' | xargs)"
 