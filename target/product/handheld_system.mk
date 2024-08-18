b18ae561a8467a4f712692a36f90e5f6ea5c8c8f
yehonatan2020 <yehonatan@yandex.ru>
2024-08-18 22:19:31 +0100

    Remove those things we won't need

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -47,7 +47,6 @@ PRODUCT_PACKAGES += \
     DeviceAsWebcam \
     DocumentsUI \
     DownloadProviderUi \
-    EasterEgg \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
@@ -96,4 +95,3 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Traceur for debug only
 PRODUCT_PACKAGES_ENG += \
-    Traceur
